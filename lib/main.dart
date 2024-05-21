22737750c144020ccc5f73c18edcff89480014f7
Bala <balasubramaniam.s@5paisa.com>
2024-05-21 19:51:43 +0530

    INIT

--- lib/main.dart
+++ lib/main.dart
@@ -65,36 +65,6 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("sample"),
-      ),
-      body: InkWell(
-        child: Container(
-          color: Colors.red,
-          constraints: BoxConstraints.expand(),
-          child: Column(
-            children: <Widget>[
-              RaisedButton(
-                child: Text("present ios view controller"),
-                onPressed: () async {
-                  final String res = await _channel.invokeMethod("foo");
-                },
-              ),
-            ],
-          ),
-        ),
-        onTap: () {
-          showDialog(
-              context: context,
-              builder: (context) => AlertDialog(title: Text("flutter event!")));
-        },
-      ),
-    );
-  }
-
-  /*@override
-  Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
     // by the _incrementCounter method above.
     //
@@ -110,55 +80,72 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
-        child: GestureDetector(
-          onLongPress: (){
-            Fluttertoast.showToast(
-              msg: "onLongPress",
-              toastLength: Toast.LENGTH_SHORT, // Duration for which the toast should be visible
-              gravity: ToastGravity.BOTTOM, // Toast position
-              timeInSecForIosWeb: 1, // Time for iOS-specific
-              backgroundColor: Colors.grey, // Background color of the toast
-              textColor: Colors.white, // Text color of the toast message
-              fontSize: 16.0, // Font size of the toast message
-            );
-          },
-          onTap: (){
-            Fluttertoast.showToast(
-              msg: "onTap",
-              toastLength: Toast.LENGTH_SHORT, // Duration for which the toast should be visible
-              gravity: ToastGravity.BOTTOM, // Toast position
-              timeInSecForIosWeb: 1, // Time for iOS-specific
-              backgroundColor: Colors.grey, // Background color of the toast
-              textColor: Colors.white, // Text color of the toast message
-              fontSize: 16.0, // Font size of the toast message
-            );
-          },
-          child: Column(
-            // Column is also a layout widget. It takes a list of children and
-            // arranges them vertically. By default, it sizes itself to fit its
-            // children horizontally, and tries to be as tall as its parent.
-            //
-            // Invoke "debug painting" (press "p" in the console, choose the
-            // "Toggle Debug Paint" action from the Flutter Inspector in Android
-            // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-            // to see the wireframe for each widget.
-            //
-            // Column has various properties to control how it sizes itself and
-            // how it positions its children. Here we use mainAxisAlignment to
-            // center the children vertically; the main axis here is the vertical
-            // axis because Columns are vertical (the cross axis would be
-            // horizontal).
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              const Text(
-                'You have pushed the button this many times:',
-              ),
-              Text(
-                '$_counter',
-                style: Theme.of(context).textTheme.headlineMedium,
-              ),
-            ],
-          ),
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Invoke "debug painting" (press "p" in the console, choose the
+          // "Toggle Debug Paint" action from the Flutter Inspector in Android
+          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+          // to see the wireframe for each widget.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            InkWell(
+                onLongPress: () {
+                  Fluttertoast.showToast(
+                    msg: "onLongPress",
+                    toastLength: Toast.LENGTH_SHORT,
+                    // Duration for which the toast should be visible
+                    gravity: ToastGravity.BOTTOM,
+                    // Toast position
+                    timeInSecForIosWeb: 1,
+                    // Time for iOS-specific
+                    backgroundColor: Colors.grey,
+                    // Background color of the toast
+                    textColor: Colors.white,
+                    // Text color of the toast message
+                    fontSize: 16.0, // Font size of the toast message
+                  );
+                },
+                onTap: () {
+                  Fluttertoast.showToast(
+                    msg: "onTap",
+                    toastLength: Toast.LENGTH_SHORT,
+                    // Duration for which the toast should be visible
+                    gravity: ToastGravity.BOTTOM,
+                    // Toast position
+                    timeInSecForIosWeb: 1,
+                    // Time for iOS-specific
+                    backgroundColor: Colors.grey,
+                    // Background color of the toast
+                    textColor: Colors.white,
+                    // Text color of the toast message
+                    fontSize: 16.0, // Font size of the toast message
+                  );
+                },
+                child: const SizedBox(
+                  width: 200,
+                  height: 100,
+                  child: ColoredBox(
+                    color: Colors.blue,
+                    child: Center(child: Text('Click me')),
+                  ),
+                ))
+          ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
@@ -167,5 +154,5 @@ class _MyHomePageState extends State<MyHomePage> {
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
-  }*/
+  }
 }
